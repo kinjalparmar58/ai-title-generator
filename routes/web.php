35fb3b5dfdbf33f0591cbdf4d1ffa09b6b13ceff
Kinjal Parmar <94967864+kinjalparmar58@users.noreply.github.com>
2025-03-23 15:57:20 +0530

    Update web.php
    Routes have been added.

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\ChatController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +17,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::post('/chat', [ChatController::class, 'generate'])->name('title.generate');
